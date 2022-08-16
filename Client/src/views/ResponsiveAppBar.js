71e7d9be7a9f638207eed5cf7950a77cd0358c7f
FaroukAmr <faroukamr508@gmail.com>
2022-08-16 10:44:40 +0200

    tweaks-git-ignore

--- Client/src/views/ResponsiveAppBar.js
+++ Client/src/views/ResponsiveAppBar.js
@@ -19,6 +19,11 @@ import cookies from 'js-cookie';
 import axios from 'axios';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import '../css/Navbar.css';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 const languages = [
   {
     code: 'en',
@@ -37,7 +42,6 @@ const languages = [
     country_code: 'fr',
   },
 ];
-const settings = ['Profile', 'Account', 'Logout'];
 const ResponsiveAppBar = () => {
   const currentLanguageCode = cookies.get('i18next') || 'en';
   const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
@@ -61,27 +65,11 @@ const ResponsiveAppBar = () => {
     }
   }, [rendersignupButton, currentLanguage]);
 
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   const logoutHandler = () => {
     localStorage.removeItem('authToken');
     navigate('/login');
   };
 
-  const handleMenuClick = (req) => {
-    if (req === 'Logout') {
-      logoutHandler();
-      return;
-    }
-  };
   const token = localStorage.getItem('authToken');
 
   const config = {
@@ -111,6 +99,15 @@ const ResponsiveAppBar = () => {
     setAnchorEl(null);
   };
 
+  const [anchorElLang, setAnchorElLang] = React.useState(null);
+  const openLang = Boolean(anchorElLang);
+  const handleClickLang = (event) => {
+    setAnchorElLang(event.currentTarget);
+  };
+  const handleCloseLang = () => {
+    setAnchorElLang(null);
+  };
+
   return (
     <AppBar position="sticky">
       <div className="nvbar">
@@ -125,55 +122,6 @@ const ResponsiveAppBar = () => {
                   navigate('/');
                 }}
               />
-              <Typography
-                className="hover"
-                variant="h6"
-                noWrap
-                sx={{
-                  mr: 1,
-                  ml: 1,
-                  mt: 0,
-                  textDecoration: 'none',
-                }}
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-              >
-                {t('language')}
-                <ArrowDropDownIcon />
-              </Typography>
-              <Menu
-                id="language-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  'aria-labelledby': 'basic-button',
-                }}
-              >
-                {languages.map(({ code, name, country_code }) => (
-                  <MenuItem
-                    key={code}
-                    onClick={() => {
-                      handleClose();
-                      i18next.changeLanguage(code);
-                    }}
-                    disabled={code === currentLanguageCode}
-                  >
-                    <Typography textAlign="center">
-                      <span
-                        className={`fi fi-${country_code} mx-2`}
-                        style={{
-                          opacity: code === currentLanguageCode ? 0.5 : 1,
-                        }}
-                      ></span>
-                      {name}
-                    </Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
             </div>
             <div className="nv-3">
               {signupButton ? (
@@ -193,45 +141,147 @@ const ResponsiveAppBar = () => {
                   </Button>
                 </>
               ) : (
-                <Box>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      {image ? (
-                        <Avatar alt={name} src={image} />
-                      ) : (
-                        <Avatar>{name.charAt(0)}</Avatar>
-                      )}
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
+                <React.Fragment>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      textAlign: 'center',
                     }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
                   >
-                    {settings.map((setting) => (
-                      <MenuItem
-                        key={setting}
-                        onClick={() => {
-                          handleCloseUserMenu();
-                          handleMenuClick(setting);
+                    <Tooltip title={t('account_settings')}>
+                      <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ ml: 2 }}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                      >
+                        <Avatar sx={{ width: 32, height: 32 }} src={image}>
+                          {name.charAt(0)}
+                        </Avatar>
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    // onClick={handleClose}
+                    PaperProps={{
+                      elevation: 0,
+                      sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                          width: 32,
+                          height: 32,
+                          ml: -0.5,
+                          mr: 1,
+                        },
+                        '&:before': {
+                          content: '""',
+                          display: 'block',
+                          position: 'absolute',
+                          top: 0,
+                          right: 14,
+                          width: 10,
+                          height: 10,
+                          bgcolor: 'background.paper',
+                          transform: 'translateY(-50%) rotate(45deg)',
+                          zIndex: 0,
+                        },
+                      },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <Avatar sx={{ width: 32, height: 32 }} src={image}>
+                        {name.charAt(0)}
+                      </Avatar>
+                      <span style={{ marginRight: '1em' }}>{t('profile')}</span>
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem>
+                      <ListItemIcon onClick={handleClickLang}>
+                        <ArrowDropDownIcon fontSize="small" />
+                      </ListItemIcon>
+
+                      <Typography
+                        variant="inherit"
+                        id="basic-button"
+                        aria-controls={openLang ? 'language-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={openLang ? 'true' : undefined}
+                        onClick={handleClickLang}
+                        style={{ width: '100%' }}
+                      >
+                        {t('language')}
+                      </Typography>
+                      <Menu
+                        id="language-menu"
+                        anchorEl={anchorElLang}
+                        open={openLang}
+                        onClose={handleCloseLang}
+                        MenuListProps={{
+                          'aria-labelledby': 'basic-button',
                         }}
                       >
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    ))}
+                        {languages.map(({ code, name, country_code }) => (
+                          <MenuItem
+                            key={code}
+                            onClick={() => {
+                              handleCloseLang();
+                              handleClose();
+                              i18next.changeLanguage(code);
+                            }}
+                            disabled={code === currentLanguageCode}
+                          >
+                            <Typography textAlign="center">
+                              <ListItemIcon>
+                                <span
+                                  className={`fi fi-${country_code}`}
+                                  style={{
+                                    opacity:
+                                      code === currentLanguageCode ? 0.5 : 1,
+                                  }}
+                                ></span>
+                              </ListItemIcon>
+                              {name}
+                            </Typography>
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                    </MenuItem>
+                    <MenuItem>
+                      <ListItemIcon>
+                        <PersonAdd fontSize="small" />
+                      </ListItemIcon>
+                      {t('lorem4')}
+                    </MenuItem>
+                    <MenuItem>
+                      <ListItemIcon>
+                        <Settings fontSize="small" />
+                      </ListItemIcon>
+                      {t('settings')}
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        handleClose();
+                        logoutHandler();
+                      }}
+                    >
+                      <ListItemIcon>
+                        <Logout fontSize="small" />
+                      </ListItemIcon>
+                      {t('log_out')}
+                    </MenuItem>
                   </Menu>
-                </Box>
+                </React.Fragment>
               )}
             </div>
           </Toolbar>
