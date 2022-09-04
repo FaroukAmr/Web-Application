5f614ead4c9ece6c046d423623467863d5570648
FaroukAmr <faroukamr508@gmail.com>
2022-09-04 16:10:48 +0200

    translation-addition

--- Client/src/views/Spinner.js
+++ Client/src/views/Spinner.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
+import { useTranslation } from 'react-i18next';
 
 function Spinner() {
+  const { t } = useTranslation();
   const color = '#000000';
   useEffect(() => {});
   return (
     <div className="sweet-loading">
       <ClimbingBoxLoader color={color} loading={true} size={20} />
-      <span className="sweet-loading-txt">Loading...</span>
+      <span className="sweet-loading-txt">{t('loading')}</span>
     </div>
   );
 }
