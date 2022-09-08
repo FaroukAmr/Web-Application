25b281c273696a69eb263490bcc1dc3883b88c57
FaroukAmr <faroukamr508@gmail.com>
2022-09-08 13:47:38 +0200

    demo tweaks

--- Server/controllers/demo.js
+++ Server/controllers/demo.js
@@ -1,17 +1,20 @@
 import Card from '../models/Card.js';
 import ErrorResponse from '../utils/errorResponse.js';
-import User from '../models/User.js';
 import Lock from '../models/Lock.js';
 
 export async function getDemo(req, res, next) {
-  const userId = 'faroukzach@gmail.com';
-  const lock = await Lock.findOne({ userId });
-  let cards = await Card.find({ userId }).select('cardNumber');
-  let tempOutput = [];
-  for (let i = 0; i < cards.length; i++) {
-    tempOutput.push(cards[i].cardNumber);
+  try {
+    const userId = 'faroukzach@gmail.com';
+    const lock = await Lock.findOne({ userId });
+    let cards = await Card.find({ userId }).select('cardNumber');
+    let tempOutput = [];
+    for (let i = 0; i < cards.length; i++) {
+      tempOutput.push(cards[i].cardNumber);
+    }
+    res
+      .status(200)
+      .json({ success: true, data: { lock: lock._id, cards: tempOutput } });
+  } catch (error) {
+    return next(new ErrorResponse(error, 500));
   }
-  res
-    .status(200)
-    .json({ success: true, data: { lock: lock._id, cards: tempOutput } });
 }
