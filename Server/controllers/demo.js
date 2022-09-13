594092ff8a1cc40c8a14920fee7cda25be8429b1
FaroukAmr <faroukamr508@gmail.com>
2022-09-13 15:58:42 +0200

    some demo tweaks

--- Server/controllers/demo.js
+++ Server/controllers/demo.js
@@ -1,18 +1,21 @@
 import Card from '../models/Card.js';
-import Lock from '../models/Lock.js';
 
+export const CLIENTS = [];
 export async function wsDemo(ws) {
   try {
     const userId = 'faroukzach@gmail.com';
-    const lock = await Lock.findOne({ userId });
     let cards = await Card.find({ userId }).select('cardNumber');
     let tempOutput = [];
     for (let i = 0; i < cards.length; i++) {
       tempOutput.push(Number(cards[i].cardNumber).toString(16).toUpperCase());
     }
     tempOutput = JSON.stringify(tempOutput);
-    return ws.send(tempOutput);
-  } catch (error) {
-    return ws.send(error);
+    ws.send(`UPDATE ${tempOutput}`);
+  } catch {
+    return ws.send('Internal Server Error');
   }
 }
+
+// export function clientsConnected() {
+//   return CLIENTS.length !== 0;
+// }
